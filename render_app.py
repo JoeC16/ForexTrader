47eb4aec3038f7f4c9c79bbc8eee6a93b6fb646f
JoeC16 <joecraig16@hotmail.co.uk>
2025-07-07 23:15:49 +0100

    Update render_app.py

--- render_app.py
+++ render_app.py
@@ -52,4 +52,4 @@ def enrich_file():
     return send_file(output, mimetype='text/csv', as_attachment=True, download_name='enriched_trades.csv')
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
